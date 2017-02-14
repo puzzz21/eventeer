ab7a73c901fe25bba69e8dcdc7844d7def9ccd28
puzzz21 <puzz.mhzn@gmail.com>
2017-02-14 15:13:18 +0545

    the project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,17 +1,4 @@
-<?php namespace App\Http\Controllers;
-
-use App\Http\Requests;
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+                          
         $this->middleware('auth');
     }
 
